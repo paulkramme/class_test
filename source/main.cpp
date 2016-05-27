0e57811a73b73e97959bf007b4ee981124111254
Paul Kramme <pjkramme@gmail.com>
2016-05-27 22:06:25 +0200

    Add second value

--- source/main.cpp
+++ source/main.cpp
@@ -19,8 +19,10 @@ void square::set_value(int x, int y)
 
 int main()
 {
-     square myrect; //defining the "variable"
+     square myrect, mysecondrect; //defining the "variable"
      myrect.set_value(5, 70);
+     mysecondrect.set_value(8934404, 4513453);
      std::cout << myrect.area() << std::endl;
+     std::cout << mysecondrect.area() << std::endl;
      return 0;
 }
