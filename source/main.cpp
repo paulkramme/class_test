b1923c0b70c574f091be2f7b0ffb61ab4b89fd60
Paul Kramme <pjkramme@gmail.com>
2016-05-27 22:28:52 +0200

    Add constructor

--- source/main.cpp
+++ source/main.cpp
@@ -4,24 +4,23 @@ class square
 {
      int width, height;
      public:
-     void set_value (int, int);
+     square(int, int);
      int area()
      {
           return width * height;
      }
 };
 
-void square::set_value(int x, int y)
+square::square(int a, int b)
 {
-     width = x;
-     height = y;
+     width = a;
+     height = b;
 }
 
 int main()
 {
-     square myrect, mysecondrect; //defining the "variable"
-     myrect.set_value(5, 70);
-     mysecondrect.set_value(8934404, 4513453);
+     square myrect(4, 5983);
+     square mysecondrect(454, 8539); //defining the "variable"
      std::cout << myrect.area() << std::endl;
      std::cout << mysecondrect.area() << std::endl;
      return 0;
